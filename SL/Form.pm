6630d55185750b57702f5ecc6d266f872d867846
Tekki <tekki@tekki.ch>
2025-02-19 10:55:57 +0100

    Bump version to 3.2.12.55.

--- SL/Form.pm
+++ SL/Form.pm
@@ -127,7 +127,7 @@ sub new {
 
   $self->{version} = "3.2.12";
   $self->{dbversion} = "3.2.4";
-  $self->{version2} = "tekki 3.2.12.54";
+  $self->{version2} = "tekki 3.2.12.55";
   $self->{dbversion2} = 49;
   $self->{cssversion} = 53;
 
