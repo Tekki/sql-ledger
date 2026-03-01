b1f0b1e7e337b04d7f43f3aaa571c7c1eebd1091
Tekki <tekki@tekki.ch>
2026-03-01 06:35:07 +0100

    Prepare next version.

--- SL/Form.pm
+++ SL/Form.pm
@@ -134,7 +134,7 @@ sub new ($type, $userspath = '') {
 
   $self->{charset} = 'UTF-8';
 
-  $self->{version}    = '4.0.6';
+  $self->{version}    = '4.0.7_01';
   $self->{dbversion}  = '4.0.0';
   $self->{cssversion} = 53;
 
